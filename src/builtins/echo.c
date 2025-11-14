1d1fa654a21e796b81822d9b077e71c1228e337d
elbrunis <brunoiniestabarrera@gmail.com>
2025-11-14 11:05:24 +0100

    he pasado echo.h a norminette

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -3,44 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maximo <maximo@student.42.fr>              +#+  +:+       +#+        */
+/*   By: runo <runo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 20:51:03 by mwilline          #+#    #+#             */
-/*   Updated: 2025/11/13 00:00:15 by maximo           ###   ########.fr       */
+/*   Updated: 2025/11/14 11:03:13 by runo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <stdio.h>
 
-int builtin_echo(char **args, t_data *data)
+static void	print_args(char **args, int i)
 {
-    int i = 1;
-    int newline = 1;
+	while (args[i])
+	{
+		ft_putstr_fd(args[i], STDOUT_FILENO);
+		if (args[i + 1])
+			ft_putchar_fd(' ', STDOUT_FILENO);
+		i++;
+	}
+}
 
-    // Soporte para múltiples -n
-    while (args[i] && args[i][0] == '-' && args[i][1] == 'n')
-    {
-        int j = 1;
-        while (args[i][j] == 'n')
-            j++;
-        if (args[i][j] != '\0')
-            break;
-        newline = 0;
-        i++;
-    }
+int	builtin_echo(char **args, t_data *data)
+{
+	int	i;
+	int	newline;
+	int	j;
 
-    // Ya no expandas aquí, los args ya vienen procesados
-    while (args[i])
-    {
-        ft_putstr_fd(args[i], STDOUT_FILENO);
-        if (args[i + 1])
-            ft_putchar_fd(' ', STDOUT_FILENO);
-        i++;
-    }
-
-    if (newline)
-        ft_putchar_fd('\n', STDOUT_FILENO);
-    data->exit_status = 0;
-    return (0);
+	i = 1;
+	newline = 1;
+	while (args[i] && args[i][0] == '-' && args[i][1] == 'n')
+	{
+		j = 1;
+		while (args[i][j] == 'n')
+			j++;
+		if (args[i][j] != '\0')
+			break ;
+		newline = 0;
+		i++;
+	}
+	print_args(args, i);
+	if (newline)
+		ft_putchar_fd('\n', STDOUT_FILENO);
+	data->exit_status = 0;
+	return (0);
 }
