8fd3d99a9dad5cbc554de9928d525d798d1489ad
Maximo Williner <mwilline@c2r11s5.42madrid.com>
2025-11-17 19:04:34 +0100

    Norma de main y de .h

--- inc/minishell.h
+++ inc/minishell.h
@@ -4,28 +4,29 @@
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maximo <maximo@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*                                                +#+#+#+#+#+  	+#+           */
 /*   Created: 2025/07/08 16:07:42 by mwilline          #+#    #+#             */
 /*   Updated: 2025/07/22 21:05:58 by maximo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-#define MINISHELL_H
+# define MINISHELL_H
 
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <unistd.h>
-#include <libft.h>
-#include "message.h"
+# include "message.h"
 # include <fcntl.h>
+# include <libft.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
 # include <string.h>
 # include <sys/wait.h>
+# include <unistd.h>
 
-typedef struct s_env{
+typedef struct s_env
+{
 	char *key;
 	char *value;
 	struct s_env *next;
@@ -40,36 +41,36 @@ typedef struct s_parse_token
 
 typedef struct s_token
 {
-	int				type;
-	char			*str;
-	
-	int				infile;
-	int				outfile;
-	int				heredoc;
-	int				append; 
-	
-	struct t_data	*data;
-	struct s_token	*next;
-}	t_token;
+	int type;
+	char *str;
+
+	int infile;
+	int outfile;
+	int heredoc;
+	int append;
+
+	struct t_data *data;
+	struct s_token *next;
+} t_token;
 
 typedef struct t_data
 {
 	t_token *token;
-	t_env	*env;
-	pid_t	*pid;
+	t_env *env;
+	pid_t *pid;
 
-	int		pipe;
-	int		exit_status;//
+	int pipe;
+	int exit_status; //
 
-	char	***cmd;
-	char	**single_cmd;
+	char ***cmd;
+	char **single_cmd;
 } t_data;
 
 void	execute_cmd(t_env *env, char **cmd);
-int		find_valid_path(char **paths, char **envp, char **cmd);
+int	find_valid_path(char **paths, char **envp, char **cmd);
 char	*get_envp(char **envp);
 char	**get_cmds(t_data *data);
-int		ft_here_doc(char *limit);
+int	ft_here_doc(char *limit);
 
 // redir
 void	ft_father(t_data *data, t_env *envp);
@@ -82,7 +83,7 @@ t_token	*ft_token_clone_range(t_token *start, t_token *end);
 
 // pipes
 void	setup_child_pipes(int **pipes, int n_pipes, int index);
-int		**create_pipes(int n_pipes);
+int	**create_pipes(int n_pipes);
 void	close_all_pipes(int **pipes, int n_pipes);
 void	free_pipes(int **pipes, int n_pipes);
 
@@ -91,12 +92,12 @@ void	split_arg(char *args, t_data *data);
 void	put_lstback(t_token **head, t_token *new);
 void	get_token_type(char *arg, t_token *token, t_data *data);
 t_token	*ft_token_new(char *str);
-int		is_builtin(const char *str);
+int	is_builtin(const char *str);
 
 // quote_handle
-t_parse_token *split_with_quotes(char *str);
+t_parse_token	*split_with_quotes(char *str);
 char	*expand_variables(char *str, t_data *data);
-char **process_tokens(t_parse_token *tokens, t_data *data);
+char	**process_tokens(t_parse_token *tokens, t_data *data);
 
 // utils
 void	ft_error(char *str, int quit);
@@ -104,19 +105,19 @@ void	free_split(char **str);
 void	free_and_exit(char *str, char **envp, int exit);
 void	free_list(t_token *head);
 void	ft_free(char *str, char **envp);
-int		count_pipe_args(t_token *token);
+int	count_pipe_args(t_token *token);
 void	free_args(t_data *data);
-int		count_commands(t_token *token);
+int	count_commands(t_token *token);
 void	ft_free_list(t_token **token);
 void	free_env_list(t_env *env);
 void	free_cmd_array(char ***cmd_array);
-int		ft_strcmp(const char *s, const char *ss);
-void free_token(t_parse_token *token);
+int	ft_strcmp(const char *s, const char *ss);
+void	free_token(t_parse_token *token);
 char	*expand_input(char *input);
-int		get_var_name_len(char *str);
+int	get_var_name_len(char *str);
 char	*get_var_name(char *str, int len);
-int builtin_modifies_state(char *cmd);
-int has_redirection(t_token *token);
+int	builtin_modifies_state(char *cmd);
+int	has_redirection(t_token *token);
 
 // signs
 void	set_signal(void);
@@ -132,15 +133,15 @@ char	**env_list_to_array(t_env *env);
 void	unset_env_value(t_env **env, const char *key);
 
 // build
-int		execute_builtin(t_data *data, t_env **env);
-int		builtin_cd(t_token *token, t_env *env);
-int		builtin_echo(char **args, t_data *data);
-int		builtin_env(t_env *env);
-int		builtin_exit(char **args);
-int		ft_isdigit_str(char *str);
-int		builtin_export(char **args, t_env **env);
-int		builtin_pwd(void);
-int		builtin_unset(char **args, t_env **env);
+int	execute_builtin(t_data *data, t_env **env);
+int	builtin_cd(t_token *token, t_env *env);
+int	builtin_echo(char **args, t_data *data);
+int	builtin_env(t_env *env);
+int	builtin_exit(char **args);
+int	ft_isdigit_str(char *str);
+int	builtin_export(char **args, t_env **env);
+int	builtin_pwd(void);
+int	builtin_unset(char **args, t_env **env);
 
 void	mini_init(t_data *data, t_env *envp);
 void	ft_take_cmd(t_data *data);
@@ -148,7 +149,7 @@ void	take_single_cmd(t_data *data);
 void	print_cmds(t_data *data);
 
 void	redir_cmd(t_data *data, t_env *envp);
-int alloc_cmd_array(t_data *data);
+int	alloc_cmd_array(t_data *data);
 char	**alloc_subcmd(t_token *current);
 void	fill_subcmd(t_token **cur, char **subcmd);
 
@@ -160,8 +161,5 @@ void	handle_path_search(char **paths, char **envp_array, char **cmd);
 //exec
 int	is_path(char *cmd);
 void	execute_direct_path(char **cmd, char **envp_array);
-
-
-
 
 #endif
--- src/main.c
+++ src/main.c
@@ -2,9 +2,12 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: maximo <maximo@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*                                                    +:+ +:+        
+	+:+     */
+/*   By: maximo <maximo@student.42.fr>              +#+  +:+      
+	+#+        */
+/*                                                +#+#+#+#+#+  
+	+#+           */
 /*   Created: 2025/07/08 16:06:53 by mwilline          #+#    #+#             */
 /*   Updated: 2025/07/22 21:19:12 by maximo           ###   ########.fr       */
 /*                                                                            */
@@ -12,10 +15,11 @@
 
 #include "minishell.h"
 
-int main(int argc, char **argv, char **envp)
+
+int	main(int argc, char **argv, char **envp)
 {
-	char	*input;
-	t_data	data;
+	char *input;
+	t_data data;
 	(void)argc;
 	(void)argv;
 	data.token = NULL;
@@ -23,61 +27,61 @@ int main(int argc, char **argv, char **envp)
 	data.exit_status = 0;
 
 	set_signal();
-	while(1)
+	while (1)
 	{
 		input = readline("minishell$ ");
-		if(!input)
-			break;
-		if(*input)
+		if (!input)
+			break ;
+		if (*input)
 			add_history(input);
 		data.token = NULL;
 		data.pipe = 0;
-		if(input[0])
+		if (input[0])
 		{
-		split_arg(input, &data);
-		mini_init(&data, data.env);
-		if (data.token)
-    		free_list(data.token);
+			split_arg(input, &data);
+			mini_init(&data, data.env);
+			if (data.token)
+				free_list(data.token);
 		}
 		free(input);
 	}
 	free_env_list(data.env);
-	return(0);
+	return (0);
 }
 
-void mini_init(t_data *data, t_env *envp)
+void	mini_init(t_data *data, t_env *envp)
 {
-    ft_take_cmd(data);
-    if (!data->token)
-        return;
+	ft_take_cmd(data);
+	if (!data->token)
+		return ;
 
-    if (data->pipe == 0)
+	if (data->pipe == 0)
 		redir_cmd(data, envp);
-    else
-    {
-        execute_pipes(data, envp);
-        free_args(data);
-    }
-    if (data->cmd)
-    {
-        free_cmd_array(data->cmd);
-        data->cmd = NULL;
-    }
-    if (data->token)
-    {
-        free_list(data->token);
-        data->token = NULL;
-    }
+	else
+	{
+		execute_pipes(data, envp);
+		free_args(data);
+	}
+	if (data->cmd)
+	{
+		free_cmd_array(data->cmd);
+		data->cmd = NULL;
+	}
+	if (data->token)
+	{
+		free_list(data->token);
+		data->token = NULL;
+	}
 }
 
 void	ft_take_cmd(t_data *data)
 {
-	t_token	*current;
-	int		i;
-	char	**subcmd;
+	t_token *current;
+	int i;
+	char **subcmd;
 
 	if (!alloc_cmd_array(data))
-		return;
+		return ;
 	current = data->token;
 	i = 0;
 	while (current)
@@ -93,51 +97,3 @@ void	ft_take_cmd(t_data *data)
 	data->cmd[i] = NULL;
 }
 
-// void	ft_take_cmd(t_data *data)
-// {
-// 	t_token	*current;
-// 	int		i;
-// 	int		j;
-// 	char	**subcmd;
-// 	int		arg_count;
-
-// 	if (!data || !data->token)
-// 		return;
-
-// 	data->cmd = malloc(sizeof(char **) * (data->pipe + 2));
-// 	if (!data->cmd)
-// 		return;
-// 	i = 0;
-// 	current = data->token;
-
-// 	while (current)
-// 	{
-// 		j = 0;
-// 		arg_count = count_pipe_args(current);
-// 		if (arg_count <= 0)
-// 			arg_count = 1;
-// 		subcmd = malloc(sizeof(char *) * (arg_count + 1));
-// 		if (!subcmd)
-// 			return (free_cmd_array(data->cmd), (void)(data->cmd = NULL));
-
-// 		while (current && current->type != PIPE)
-// 		{
-// 			if (current->type == OUTFILE || current->type == INFILE
-// 				|| current->type == APPEND || current->type == HEREDOC)
-// 			{
-// 				current = current->next;
-// 				if (current)
-// 					current = current->next;
-// 				continue;
-// 			}
-// 			subcmd[j++] = ft_strdup(current->str);
-// 			current = current->next;
-// 		}
-// 		subcmd[j] = NULL;
-// 		data->cmd[i++] = subcmd;
-
-// 		if (current && current->type == PIPE)
-// 			current = current->next;
-// 	}
-// 	data->cmd[i] = NULL;
-// }
